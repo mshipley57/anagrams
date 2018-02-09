ae27c042a9aefd329d9ecfdfb7c0150c831d4e39
Mariah Shipley <mshipley57@gmail.com>
2018-02-09 15:04:17 -0800

    add tests

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,44 +1,21 @@
-class Anagram
-  def initialize(inputs)
-    @inputs = inputs
+class Anagrams
+  def initialize(words)
+    @words = words
   end
-
-  def input_to_words
-    first_word = {"a", "e", "i", "o", "u"}
-
-    second word = {"a", "e", "i", "o", "u"}
+  print "Enter two words to see if they are anagrams"
+  def letter_counter
+    letter_counter.split(" ")
   end
-  if (first_word.length) != (second_word.length)
-      print "These are not anagrams"
-  end
-
-end
-
-end
-
-
-
-
   #   return false if self.length != other.length
   #   self.sort == other.sort
   # end
 
 
-
-
-
-
-
-  def initialize(inputs)
-    @inputs = inputs
-  end
-
-  def input_to_words
-    first_word = {"a", "e", "i", "o", "u"}
-
-    second word = {"a", "e", "i", "o", "u"}
-
-    if (first_word.length) != (second_word.length)
-      print "These are not anagrams"
-    end
-end
+  # def input_to_words
+    #   first_word = {"a", "e", "i", "o", "u"}
+    #
+    #   second word = {"a", "e", "i", "o", "u"}
+    # end
+    # if (first_word.length) != (second_word.length)
+    #     print "These are not anagrams"
+    # end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,8 +1,21 @@
-require('anagrams.rb')
 require('rspec')
+require('anagrams')
 
 
-describe('#anagram_project') do
-  it("ask for two words and check if they are words)
-    words = Input.new(2)
-    expect(words.anagram_project()).to(eq("two"))
+describe('anagrams') do
+  it("ask for two words and check if they are words")
+    expect(words.anagrams()).to(eq("2"))
+end
+    # words = Input.new(2)
+describe('anagrams') do
+  it("set all letters to same case")
+    expect(words.anagrams()).to(eq("2"))
+end
+describe('anagrams') do
+  it("check for vowels")
+    expect(words.anagrams()).to(eq("2"))
+end
+describe('anagrams') do
+  it("identify antigrams")
+    expect(words.anagrams()).to(eq("2"))
+end
